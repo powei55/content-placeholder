c4dbb167046808ee21421417e3611a62f7b62a29
Ikechukwu Powei Mathias <ikechukwupowei21@gmail.com>
2022-07-03 05:00:42 +0100

    content-placeholder via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bgs_text = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+    header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"alt=""/>'
+    title.innerHTML = 'lorem ipsum dolor sit amet'
+    excerpt.innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, aspernatur!'
+    profile_img.innerHTML = ' <img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'oct 08, 2022'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg') )
+    animated_bgs_text.forEach(bg => bg.classList.remove('animated-bg') )
+
+}
